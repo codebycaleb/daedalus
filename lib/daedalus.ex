c769c9c671dd44d05a6c91af3da7ecfe0f23a163
Caleb McQuillin <codebycaleb@users.noreply.github.com>
2024-06-11 08:41:42 -0400

    Clear boilerplate code

--- lib/daedalus.ex
+++ lib/daedalus.ex
@@ -2,17 +2,4 @@ defmodule Daedalus do
   @moduledoc """
   Documentation for `Daedalus`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Daedalus.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/daedalus_test.exs
+++ test/daedalus_test.exs
@@ -1,8 +1,4 @@
 defmodule DaedalusTest do
   use ExUnit.Case
   doctest Daedalus
-
-  test "greets the world" do
-    assert Daedalus.hello() == :world
-  end
 end
