f45c9ae831f0e8dba2e2d50d38ee14860439bad4
Caleb McQuillin <codebycaleb@users.noreply.github.com>
2024-06-12 08:57:52 -0400

    Add bias option to Algorithms.BinaryTree.on/2

--- lib/algorithms/binary_tree.ex
+++ lib/algorithms/binary_tree.ex
@@ -1,19 +1,77 @@
 defmodule Algorithms.BinaryTree do
-  def on(grid) do
+  defmodule Options do
+    @type bias :: :northeast | :northwest | :southeast | :southwest
+    @type t :: [{:bias, bias()}]
+
+    @doc """
+    Validates the options for the BinaryTree algorithm.
+    """
+    def validate!(options) do
+      result =
+        Enum.reduce_while(options, options, fn {key, value}, acc ->
+          case key do
+            :bias ->
+              case value do
+                :northeast -> {:cont, acc}
+                :northwest -> {:cont, acc}
+                :southeast -> {:cont, acc}
+                :southwest -> {:cont, acc}
+                _ -> {:halt, {:error, "Invalid bias option value: #{inspect(value)}"}}
+              end
+
+            _ ->
+              {:halt, {:error, "Invalid option for BinaryTree algorithm: #{inspect(key)}"}}
+          end
+        end)
+
+      case result do
+        {:error, message} -> raise ArgumentError, message
+        _ -> result
+      end
+    end
+  end
+
+  @moduledoc """
+  The `Algorithms.BinaryTree` module contains the implementation of the binary tree algorithm for generating mazes.
+
+  The TLDR of the algorithm is:
+
+  For each cell in the grid:
+    - Determine the available neighbors of the cell (based on the bias option).
+    - Randomly link the cell to one of its neighbors (if any).
+
+  ## Examples
+
+      iex> :rand.seed(:exsss, {6, 9, 42})
+      iex> Grid.new(2, 2) |> Algorithms.BinaryTree.on(bias: :southwest) |> to_string()
+      to_string('''
+      +---+---+
+      |   |   |
+      +   +   +
+      |       |
+      +---+---+
+      ''')
+  """
+  @spec on(Grid.t()) :: Grid.t()
+  @spec on(Grid.t(), Options.t()) :: Grid.t()
+  def on(grid, options \\ [bias: :northeast]) do
+    options = Options.validate!(options)
+    bias = Keyword.get(options, :bias, :northeast)
+
     grid.cells
     |> List.flatten()
     |> Enum.reduce(grid, fn cell, acc ->
       neighbors =
-        if Grid.exists?(grid, cell.row, cell.column + 1),
-          do: [Grid.get(grid, cell.row, cell.column + 1)],
-          else: []
-
-      neighbors =
-        if Grid.exists?(grid, cell.row - 1, cell.column),
-          do: [Grid.get(grid, cell.row - 1, cell.column) | neighbors],
-          else: neighbors
+        get_options(bias)
+        |> Enum.map(fn {dx, dy} -> Grid.get(grid, cell.row + dy, cell.column + dx) end)
+        |> Enum.filter(& &1)
 
       if neighbors != [], do: Grid.link(acc, cell, Enum.random(neighbors)), else: acc
     end)
   end
+
+  defp get_options(:northeast), do: [{0, -1}, {1, 0}]
+  defp get_options(:northwest), do: [{0, -1}, {-1, 0}]
+  defp get_options(:southeast), do: [{0, 1}, {1, 0}]
+  defp get_options(:southwest), do: [{0, 1}, {-1, 0}]
 end
