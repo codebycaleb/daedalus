51e71521f8b983bc1cacd577cae96dacc47b5dce
Caleb McQuillin <codebycaleb@users.noreply.github.com>
2024-06-14 06:53:34 -0400

    Update BinaryTree docs; remove Options submodule
    The validation was accurate but heavy-handed to implement for other maze
    types. It's fine to just check for the valid options inline and raise on
    invalid options.
    
    Update the doc to better detail the algorithm and display a 3x3 maze.

--- lib/algorithms/binary_tree.ex
+++ lib/algorithms/binary_tree.ex
@@ -1,35 +1,5 @@
 defmodule Algorithms.BinaryTree do
-  defmodule Options do
-    @type bias :: :northeast | :northwest | :southeast | :southwest
-    @type t :: [{:bias, bias()}]
-
-    @doc """
-    Validates the options for the BinaryTree algorithm.
-    """
-    def validate!(options) do
-      result =
-        Enum.reduce_while(options, options, fn {key, value}, acc ->
-          case key do
-            :bias ->
-              case value do
-                :northeast -> {:cont, acc}
-                :northwest -> {:cont, acc}
-                :southeast -> {:cont, acc}
-                :southwest -> {:cont, acc}
-                _ -> {:halt, {:error, "Invalid bias option value: #{inspect(value)}"}}
-              end
-
-            _ ->
-              {:halt, {:error, "Invalid option for BinaryTree algorithm: #{inspect(key)}"}}
-          end
-        end)
-
-      case result do
-        {:error, message} -> raise ArgumentError, message
-        _ -> result
-      end
-    end
-  end
+  @type bias :: :northeast | :northwest | :southeast | :southwest
 
   @moduledoc """
   The `Algorithms.BinaryTree` module contains the implementation of the binary tree algorithm for generating mazes.
@@ -38,25 +8,37 @@ defmodule Algorithms.BinaryTree do
 
   For each cell in the grid:
     - Determine the available neighbors of the cell (based on the bias option).
-    - Randomly link the cell to one of its neighbors (if any).
+    - Randomly link the cell to one of its neighbors (if any are available).
+
+  ## Notes
+
+  As a result of the algorithm's design, two sides of the maze will be long "hallways" without any walls.
+  This design also has the side-effect that generated mazes will "flow" diagonally from one corner to the opposite corner.
+  The "flow" will be from the corner where the two long hallways meet to the opposite corner.
+
+  The two directions in the `:bias` option will determine which two sides of the maze have long hallways (e.g. `:northeast` has a hallway on the north and east sides).
 
   ## Examples
 
       iex> :rand.seed(:exsss, {6, 9, 42})
-      iex> Grid.new(2, 2) |> Algorithms.BinaryTree.on(bias: :southwest) |> to_string()
+      iex> Grid.new(3, 3) |> Algorithms.BinaryTree.on(bias: :southwest) |> to_string()
       to_string('''
-      +---+---+
-      |   |   |
-      +   +   +
-      |       |
-      +---+---+
+      +---+---+---+
+      |   |       |
+      +   +   +---+
+      |       |   |
+      +   +---+   +
+      |           |
+      +---+---+---+
       ''')
   """
   @spec on(Grid.t()) :: Grid.t()
-  @spec on(Grid.t(), Options.t()) :: Grid.t()
+  @spec on(Grid.t(), bias: bias()) :: Grid.t()
   def on(grid, options \\ [bias: :northeast]) do
-    options = Options.validate!(options)
     bias = Keyword.get(options, :bias, :northeast)
+
+    if bias not in [:northeast, :northwest, :southeast, :southwest],
+      do: raise(ArgumentError, "Invalid bias option")
 
     grid.cells
     |> List.flatten()
