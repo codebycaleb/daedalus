c75d3355471cc5c427e3c9e39dc10b49452fe890
Caleb McQuillin <codebycaleb@users.noreply.github.com>
2024-06-26 13:23:00 -0400

    Speed up RecursiveBacktracker

--- lib/grid/mazes/recursive_backtracker.ex
+++ lib/grid/mazes/recursive_backtracker.ex
@@ -13,39 +13,35 @@ defmodule Grid.Mazes.RecursiveBacktracker do
       iex> :rand.seed(:exsss, {1, 2, 4})
       iex> Grid.new(5, 5) |> Grid.Mazes.RecursiveBacktracker.on() |> to_string()
       "+---+---+---+---+---+
-      |   |               |
-      +   +   +---+---+   +
-      |   |       |   |   |
-      +   +---+---+   +   +
-      |       |       |   |
-      +   +   +   +   +   +
+      |   |       |       |
+      +   +   +   +---+   +
       |   |   |   |       |
-      +   +   +   +---+---+
-      |   |               |
+      +   +   +---+   +   +
+      |   |       |   |   |
+      +   +---+   +   +   +
+      |       |   |   |   |
+      +   +---+   +   +   +
+      |               |   |
       +---+---+---+---+---+
       "
   """
   @spec on(Grid.t()) :: Grid.t()
   def on(grid) do
     current = Enum.random(grid.cells)
-    stack = [current]
-    visited = MapSet.new([current])
-    recurse(grid, stack, visited)
+    recurse(grid, current)
   end
 
-  defp recurse(grid, [], _visited), do: grid
+  defp recurse(grid, current) do
+    grid
+    |> Grid.neighbors(current)
+    |> Enum.reject(&Map.has_key?(grid.links, &1))
+    |> Enum.shuffle()
+    |> Enum.reduce(grid, fn
+      neighbor, grid when is_map_key(grid.links, neighbor) ->
+        grid
 
-  defp recurse(grid, [current | rest] = stack, visited) do
-    neighbors = grid |> Grid.neighbors(current) |> Enum.reject(&MapSet.member?(visited, &1))
-
-    case neighbors do
-      [] ->
-        recurse(grid, rest, visited)
-
-      _ ->
-        neighbor = Enum.random(neighbors)
-        grid = Grid.link(grid, current, neighbor)
-        recurse(grid, [neighbor | stack], MapSet.put(visited, neighbor))
-    end
+      neighbor, grid ->
+        recurse(Grid.link(grid, current, neighbor), neighbor)
+    end)
   end
 end
